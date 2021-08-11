df4d7485030555845ca081a1baadd6c8eda5a2d9
careerhao <hao.career@outlook.com>
2021-08-11 21:37:03 +0800

    Add related packages and configs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  Button
+} from 'element-ui';
+
+Vue.use(Button);
 
 Vue.config.productionTip = false
 
