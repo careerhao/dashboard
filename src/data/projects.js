bbe0e821c422326bcebf224c8a2bd45dc80051ac
careerhao <hao.career@outlook.com>
2021-08-15 20:16:29 +0800

    Create charts component shared functions and fake data for testing

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,22 +4,37 @@ export const projects = {
         id: '00000000-0000-0000-0000-000000000000',
         name: 'Project1',
         layout: [
-            {"x":0,"y":0,"w":4,"h":6,"i":"0"},
-            {"x":2,"y":0,"w":4,"h":4,"i":"1"},
-            {"x":4,"y":0,"w":3,"h":5,"i":"2"},
+            {"x":0,"y":0,"w":5,"h":8,"i":"0", "chart" : {
+                "name": "Pie",
+                "type": "pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":0,"y":10,"w":10,"h":8,"i":"1", "chart" : {
+                "name": "Line Chart",
+                "type": "line-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":6,"y":8,"w":6,"h":8,"i":"2", "chart" : {
+                "name": "",
+                "type": "rose-pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
         ],
         chartOptions: [
             {
                 id: '00000000-0000-0000-0000-000000000000',
                 categories: [
                     {   id: '00000001-0000-0000-0000-000000000000',
-                        type: 'pie',
+                        type: 'pie-chart',
                         name: 'Pie Chart1',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-simple.webp?_v_=1627897138964'
                     },
                     {
                         id: '00000002-0000-0000-0000-000000000000',
-                        type: 'rose-pie',
+                        type: 'rose-pie-chart',
                         name: 'Pie Chart2',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-roseType-simple.webp?_v_=1627897138964'
                     }
@@ -54,56 +69,42 @@ export const projects = {
                 icon: 'el-icon-data-analysis'
             },
         ],
-        charts: [
-            {
-                title: {
-                    text: '某站点用户访问来源',
-                    left: 'center'
-                },
-                tooltip: {
-                    trigger: 'item'
-                },
-                legend: {
-                    orient: 'vertical',
-                    left: 'left',
-                },
-                series: [
-                    {
-                        name: '访问来源',
-                        type: 'pie',
-                        center: ['50%', '50%'],
-                        data: [
-                            {value: 1048, name: '搜索引擎'},
-                            {value: 735, name: '直接访问'},
-                            {value: 580, name: '邮件营销'},
-                            {value: 484, name: '联盟广告'},
-                            {value: 300, name: '视频广告'}
-                        ],
-                    }
-                ]
-            }
-        ]
     },
     {
         id: '00000000-0000-0000-0000-000000000001',
         name: 'Jroject2',
         layout: [
-            {"x":0,"y":0,"w":4,"h":6,"i":"0"},
-            {"x":2,"y":0,"w":4,"h":4,"i":"1"},
-            {"x":4,"y":0,"w":3,"h":5,"i":"2"},
+            {"x":0,"y":0,"w":4,"h":6,"i":"0", "chart" : {
+                "name": "Pie",
+                "type": "pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":2,"y":0,"w":4,"h":4,"i":"1", "chart" : {
+                "name": "Line Chart",
+                "type": "line-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":4,"y":0,"w":3,"h":5,"i":"2", "chart" : {
+                "name": "",
+                "type": "rose-pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
         ],
         chartOptions: [
             {
                 id: '00000000-0000-0000-0000-000000000000',
                 categories: [
                     {   id: '00000001-0000-0000-0000-000000000000',
-                        type: 'pie',
+                        type: 'pie-chart',
                         name: 'Pie Chart1',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-simple.webp?_v_=1627897138964'
                     },
                     {
                         id: '00000002-0000-0000-0000-000000000000',
-                        type: 'rose-pie',
+                        type: 'rose-pie-chart',
                         name: 'Pie Chart2',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-roseType-simple.webp?_v_=1627897138964'
                     }
@@ -143,22 +144,37 @@ export const projects = {
         id: '00000000-0000-0000-0000-000000000002',
         name: 'Prject3',
         layout: [
-            {"x":0,"y":0,"w":4,"h":6,"i":"0"},
-            {"x":2,"y":0,"w":4,"h":4,"i":"1"},
-            {"x":4,"y":0,"w":3,"h":5,"i":"2"},
+            {"x":0,"y":0,"w":4,"h":6,"i":"0", "chart" : {
+                "name": "Pie",
+                "type": "pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":2,"y":0,"w":4,"h":4,"i":"1", "chart" : {
+                "name": "Line Chart",
+                "type": "line-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":4,"y":0,"w":3,"h":5,"i":"2", "chart" : {
+                "name": "",
+                "type": "rose-pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
         ],
         chartOptions: [
             {
                 id: '00000000-0000-0000-0000-000000000000',
                 categories: [
                     {   id: '00000001-0000-0000-0000-000000000000',
-                        type: 'pie',
+                        type: 'pie-chart',
                         name: 'Pie Chart1',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-simple.webp?_v_=1627897138964'
                     },
                     {
                         id: '00000002-0000-0000-0000-000000000000',
-                        type: 'rose-pie',
+                        type: 'rose-pie-chart',
                         name: 'Pie Chart2',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-roseType-simple.webp?_v_=1627897138964'
                     }
@@ -198,22 +214,37 @@ export const projects = {
         id: '00000000-0000-0000-0000-000000000003',
         name: 'Project4',
         layout: [
-            {"x":0,"y":0,"w":4,"h":6,"i":"0"},
-            {"x":2,"y":0,"w":4,"h":4,"i":"1"},
-            {"x":4,"y":0,"w":3,"h":5,"i":"2"},
+            {"x":0,"y":0,"w":4,"h":6,"i":"0", "chart" : {
+                "name": "Pie",
+                "type": "pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":2,"y":0,"w":4,"h":4,"i":"1", "chart" : {
+                "name": "Line Chart",
+                "type": "line-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
+            {"x":4,"y":0,"w":3,"h":5,"i":"2", "chart" : {
+                "name": "",
+                "type": "rose-pie-chart",
+                "url": "www.testurl.com/api/",
+                "fetchBySql": false,
+            }},
         ],
         chartOptions: [
             {
                 id: '00000000-0000-0000-0000-000000000000',
                 categories: [
                     {   id: '00000001-0000-0000-0000-000000000000',
-                        type: 'pie',
+                        type: 'pie-chart',
                         name: 'Pie Chart1',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-simple.webp?_v_=1627897138964'
                     },
                     {
                         id: '00000002-0000-0000-0000-000000000000',
-                        type: 'rose-pie',
+                        type: 'rose-pie-chart',
                         name: 'Pie Chart2',
                         thumburl: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-roseType-simple.webp?_v_=1627897138964'
                     }
